03010c6b380c7e30c3f2cdfa49d7d02b04e48038
SVEngelgem <stijn.vanengelgem@hotmail.com>
2017-11-16 23:22:47 +0100

    time display update

--- AfsprakenVanEngelgem2/app/src/main/java/com/example/stijn/afsprakenvanengelgem/MainActivity.java
+++ AfsprakenVanEngelgem2/app/src/main/java/com/example/stijn/afsprakenvanengelgem/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.stijn.afsprakenvanengelgem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewDebug;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Objects;
 
@@ -25,7 +28,23 @@ public class MainActivity extends AppCompatActivity {
         final List<Appointment> arrayofappointments = new AppointmentDummyRepository().getAppointments();
 
         //header opmaak
-        final Appointment appointment = arrayofappointments.get(0);
+        //implementatie van if en else kan veel beter hier
+        long currentime = System.currentTimeMillis();
+        long lowestTimeDif =Long.MAX_VALUE;
+        long timeDif;
+        int firstDate = 0;
+        for (int t = 0; t < arrayofappointments.size();t++ ){
+            final Appointment appointment = arrayofappointments.get(t);
+            long appointmentTime = appointment.getTime();
+            if(currentime < appointmentTime){
+                timeDif = appointmentTime - currentime;
+                if (timeDif < lowestTimeDif){
+                    lowestTimeDif = timeDif;
+                    firstDate = t;
+                }
+            }
+        }
+        final Appointment appointment = arrayofappointments.get(firstDate);
         TextView header = (TextView)findViewById(R.id.main_header);
         header.setText("You're next appointment is with "+appointment.getContactName()+" at "+appointment.getDay()+"/"+appointment.getMonth()+" "+appointment.getHour()+":"+appointment.getMinute());
 
