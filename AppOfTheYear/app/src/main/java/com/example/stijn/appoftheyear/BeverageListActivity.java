aaaae3947a9b6fced569ed8bd4bcb769ba93d91b
SVEngelgem <stijn.vanengelgem@hotmail.com>
2017-11-29 23:08:57 +0100

    update aanpassingssysteem updaten dranklijst

--- AppOfTheYear/app/src/main/java/com/example/stijn/appoftheyear/BeverageListActivity.java
+++ AppOfTheYear/app/src/main/java/com/example/stijn/appoftheyear/BeverageListActivity.java
@@ -1,8 +1,16 @@
 package com.example.stijn.appoftheyear;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.security.MessageDigest;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -13,11 +21,49 @@ public class BeverageListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beverage_list);
-        Realm realm = Realm.getDefaultInstance();
+        final Realm realm = Realm.getDefaultInstance();
 
         final RealmResults<Consumptie> consumpties = realm.where(Consumptie.class).findAll();
 
         ListView beverageListView = (ListView)findViewById(R.id.beverage_list);
         beverageListView.setAdapter(new BeverageListArrayAdapter(this, consumpties));
+
+        Button addBeveragelist = (Button)findViewById(R.id.beverage_add_button);
+        addBeveragelist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final AlertDialog.Builder addBeerBuilder = new AlertDialog.Builder(BeverageListActivity.this);
+                View mainView = getLayoutInflater().inflate(R.layout.dialog_addbeer, null);
+
+                //de mainview hier is nodig om te reffereren naar de juiste View waar we in bezigzijn, niet de beverage listactivity maar de AlertDialog.
+                final EditText addName = (EditText) mainView.findViewById(R.id.beverage_add_button_name);
+                final EditText addPrice = (EditText) mainView.findViewById(R.id.beverage_add_button_price);
+                Button addBeerButton = (Button) mainView.findViewById(R.id.beverage_add_button_button);
+
+                //maakt alertDialog aan om later mee te kunnen werken
+                addBeerBuilder.setView(mainView);
+                final AlertDialog dialog = addBeerBuilder.create();
+
+                addBeerButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!addName.getText().toString().isEmpty()&&!addPrice.getText().toString().isEmpty()){
+                            //omzetten waardes naar bruikbare values (als ze niet null blijken te zijn)
+                            final String newName = addName.getText().toString();
+                            final double newPrice = Double.parseDouble(addPrice.getText().toString());
+                            Consumptie consumptie = new Consumptie(newName,newPrice);
+                            realm.beginTransaction();
+                            realm.copyToRealm(consumptie);
+                            realm.commitTransaction();
+                            dialog.dismiss();
+                        }else{
+                            Toast.makeText(BeverageListActivity.this,"invalid input",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                dialog.show();
+            }
+        });
     }
 }
